6bd14d806865553c6f16cca154887484458e6a7a
Patrik Olejník <patrik.olejnk@student.tuke.sk>
2019-05-03 17:03:05 +0200

    renderAsHTML

--- gamestudio/src/main/java/sk/tuke/gamestudio/game/tetravex/olejnik/webui/WebUI.java
+++ gamestudio/src/main/java/sk/tuke/gamestudio/game/tetravex/olejnik/webui/WebUI.java
@@ -7,20 +7,29 @@ import java.util.Formatter;
 
 public class WebUI {
 
-    private Field field = null;
+    private Field playingField = new Field(3,3);
+    private Field startingField = new Field(3,3);
 
     public void processCommand(String command, String row, String column) {
     }
 
     public String renderAsHtml() {
+        //playingField.ge
         Formatter sb = new Formatter();
         sb.format("<table class='field'>\n");
-        for (int row = 0; row < field.getRowCount(); row++) {
+        for (int row = 0; row < playingField.getRowCount(); row++) {
             sb.format("<tr>\n");
-            for (int column = 0; column < field.getColumnCount(); column++) {
-                Tile tile = field.getTile(field.getPlayingField(),row, column);
+            for (int column = 0; column < playingField.getColumnCount(); column++) {
+                Tile tile = playingField.getTile(playingField.getPlayingField(),row, column);
                 sb.format("<td>\n");
                 sb.format("<a href='/tetravex?row=%d&column=%d'>", row, column);
+                sb.format("\\" + tile.getUpperNumber() + "/");
+                sb.format("<br></br>");
+                sb.format(" " + tile.getLeftNumber() + "|" + tile.getRightNumber() + " " + " ");
+                sb.format("<br></br>");
+                sb.format("/" + tile.getBottomNumber() + "\\" + " ");
+                sb.format("<br></br>");
+                sb.format("<br></br>");
                 sb.format("</a>");
             }
         }
@@ -28,15 +37,13 @@ public class WebUI {
         return sb.toString();
     }
 
-    private void createField() {
-        field = new Field(3, 3);
-    }
 
-    private void printHeader() {
-        StringBuilder sb = new StringBuilder();
-        for (int column = 0; column < field.getColumnCount(); column++) {
-            sb.append("   %d ").append(column).append(1);
-        }
 
-    }
+//    private void printHeader() {
+//        StringBuilder sb = new StringBuilder();
+//        for (int column = 0; column < field.getColumnCount(); column++) {
+//            sb.append("   %d ").append(column).append(1);
+//        }
+//
+//    }
 }
